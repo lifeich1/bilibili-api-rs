bc7a6395c24a2da2f35d1ae12ded97f075ec7b4a
lifeich1 <lifeich0@gmail.com>
2023-06-05 01:20:05 +0800

    :construction: root data, rm old tests (#4)

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,53 @@
 //! bilibili-api-rs is a rust library project got inspiration from [bilibili-api](https://github.com/Passkou/bilibili-api).
 //!
 //! ## Example
-//! ```
-//! use bilibili_api_rs::Context;
-//!
-//! #[tokio::main]
-//! async fn main() {
-//!     let n = Context::new().unwrap();
-//!     let v = n.new_user(15810).get_info().unwrap().query().await.unwrap();
-//!     assert_eq!(v["name"].as_str().unwrap(), "Mr.Quin");
-//! }
-//! ```
 
-/// Provide bilibili APIs
-pub mod api;
+struct Bench {
+    pub data: serde_json::Value,
+}
 
-mod api_info;
+impl Bench {
+    pub fn new() -> Self {
+        let user: serde_json::Value = serde_json::from_str(include_str!("api_info/user.json"))
+            .expect("api_info/user.json invalid");
+        let live: serde_json::Value = serde_json::from_str(include_str!("api_info/live.json"))
+            .expect("api_info/live.json invalid");
+        let video: serde_json::Value = serde_json::from_str(include_str!("api_info/video.json"))
+            .expect("api_info/video.json invalid");
+        let xlive: serde_json::Value = serde_json::from_str(include_str!("api_info/xlive.json"))
+            .expect("api_info/xlive.json invalid");
+        Self {
+            data: serde_json::json!({
+                "api": {
+                    "user": user,
+                    "live": live,
+                    "video": video,
+                    "xlive": xlive,
+                }
+            }),
+        }
+    }
+}
 
-/// The module declare the cache interface and provide a [`SimpleMemCacher`][cache::SimpleMemCacher]
-pub mod cache;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-pub use api::{user::User, xlive, Context};
+    #[test]
+    fn validate_wbi_user_info() {
+        let bench = Bench::new();
+        assert_eq!(
+            bench.data["api"]["user"]["info"]["info"]["wbi"].as_bool(),
+            Some(true)
+        );
+    }
+
+    #[test]
+    fn validate_method_xlive_get_list() {
+        let bench = Bench::new();
+        assert_eq!(
+            bench.data["api"]["xlive"]["info"]["get_list"]["method"].as_str(),
+            Some("GET")
+        );
+    }
+}
