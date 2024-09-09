a739badb17296be1449eb1396eeac443630ddccb
lifeich1 <lifeich0@gmail.com>
2024-09-10 02:03:17 +0800

    :tada: nix dev env

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "A rust library project got inspiration from bilibili-api.";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      ...
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        toolchain = pkgs.rustPlatform;
+        deps = with pkgs; [
+          openssl
+          sqlite
+        ];
+      in
+      {
+        # Used by `nix develop`
+        devShells.default = pkgs.mkShell {
+          buildInputs =
+            with pkgs;
+            [
+              (with toolchain; [
+                cargo
+                rustc
+                rustLibSrc
+              ])
+              clippy
+              rustfmt
+              pkg-config
+            ]
+            ++ deps;
+
+          # Specify the rust-src path (many editors rely on this)
+          RUST_SRC_PATH = "${toolchain.rustLibSrc}";
+
+          shellHook = ''
+            cargo --version
+            exec $SHELL
+          '';
+        };
+      }
+    );
+}
